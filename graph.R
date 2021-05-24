53bcf3b2f3caf6d22e8f635f3b1a0e7c3bf2e064
abdelmenem <abdo55@gmail.com>
2021-05-24 15:19:12 +0200

    Update graph.R

--- graph.R
+++ graph.R
@@ -1,11 +1,5 @@
-
-install.packages("jsonlite")
 library(jsonlite)
 
-
-list1 <- sample(1:100, 1000, replace=TRUE)
-
-exportJSON <- toJSON(list1)
-
-write(exportJSON, "abdo.json")
+list2 <- fromJSON("abdo.json")
+hist(list2)
 
