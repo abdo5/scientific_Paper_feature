9bc92d93c9b6c97684fb874a5edc5b802422105f
abdelmenem <abdo55@gmail.com>
2021-05-24 13:54:35 +0200

    install package
    I add the command to install the required package

--- graph.R
+++ graph.R
@@ -1,2 +1,5 @@
+library(jsonlite)
+
 list2 <- fromJSON("abdo.json")
 hist(list2)
+
--- random.R
+++ random.R
@@ -5,7 +5,7 @@ library(jsonlite)
 
 list1 <- sample(1:100, 1000, replace=TRUE)
 
-exportJson <- toJSON(list1)
+exportJSON <- toJSON(list1)
 
 write(exportJSON, "abdo.json")
 
